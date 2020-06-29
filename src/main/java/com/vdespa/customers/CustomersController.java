ac9ff6aba4666d2cca46a249a22717af098e1f90
Valentin Despa <valentin@vdespa.com>
2020-06-29 11:18:35 -0700

    Initial commit

---
+++ src/main/java/com/vdespa/customers/CustomersController.java
@@ -0,0 +1,27 @@
+package com.vdespa.customers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("customers")
+public class CustomersController {
+
+    @Autowired
+    private CustomerRepository repository;
+
+    @GetMapping
+    public List<Customer> geCustomers() {
+        return repository.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Customer getById(@PathVariable("id") long id) {
+        return repository.findById(id);
+    }
+}
