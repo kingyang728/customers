e9770b4a31a59992a811e5b848e0da3f264972a7
Valentin Despa <valentin@vdespa.com>
2020-06-29 19:34:51 -0700

    Bug fix

--- contract-tests/run-collections-in-directory.js
+++ contract-tests/run-collections-in-directory.js
@@ -13,7 +13,7 @@ fs.readdir('./contract-tests', function (err, files) {
     // now we iterate on each file name and call newman.run using each file name
     files.forEach(function (file) {
         newman.run({
-            collection: require(`${__dirname}/${file}`)
+            collection: require(`${__dirname}/${file}`),
             environment: require('contract-tests/Customers Service Github Docker.postman_environment.json')
         }).on('start', function (err, args) { // on start of run, log to console
             console.log(`Running a collection ${file}`);
