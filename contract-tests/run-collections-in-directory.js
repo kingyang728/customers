76d5626313445b202cb659351b874d3a6afedf10
Valentin Despa <valentin@vdespa.com>
2020-07-08 13:44:45 -0700

    Adapt script

--- contract-tests/run-collections-in-directory.js
+++ contract-tests/run-collections-in-directory.js
@@ -14,12 +14,17 @@ fs.readdir('./contract-tests', function (err, files) {
     files.forEach(function (file) {
         newman.run({
             collection: require(`${__dirname}/${file}`),
-            environment: './contract-tests/Customers Service Github Docker.postman_environment.json'
+            environment: './contract-tests/Customers Service Github Docker.postman_environment.json',
+            reporters: 'cli'
         }).on('start', function (err, args) { // on start of run, log to console
-            console.log(`Running a collection ${file}`);
+            console.log(`Running the collection ${file}`);
         }).on('done', function (err, summary) {
+
             if (err || summary.error) {
                 console.error('Collection run encountered an error.');
+            }
+            if (summary.run.failures.length > 0) {
+                console.error(`Running the collection ${file} ended with errors`);
             }
             else {
                 console.log('Collection run completed.');
